4285dd59c13c1c86594d62bd100414accb1191a1
olkasen765 <o.kassenov@eot.kz>
2024-07-09 11:57:44 -0500

    SQL lessons

--- day04.02-SQL.sql
+++ day04.02-SQL.sql
@@ -18,9 +18,59 @@ INSERT INTO workers VALUES(108, 'Hasan Basri', 16000);
 INSERT INTO workers VALUES(109, 'Fatih Tamer', 18000);
 INSERT INTO workers VALUES(110, 'Emel Ebru', 8000);
 
+SELECT * FROM workers;
+
+-- FIND THE MINIMUM SALARY
+
+SELECT MIN(worker_salary)
+FROM workers;
+
+-- FIND THE MINIMUM SALARY
+
+SELECT MAX(worker_salary)
+FROM workers;
+
+
+----- ALIAS ------
+
+-- We use AS(ALIAS) to give a temporary name to to the field that contains result ...
+-- IT IS LIKE system.out.println("Total Salary" + counter);
+
+
+SELECT worker_salary || ' ' || worker_name AS min_Value
+FROM workers;
+
+-- FIND THE MINIMUM SALARY
+SELECT MIN(worker_salary) AS minimum_salary
+FROM workers;
+
+-- FIND THE MAXIMUM SALARY
+SELECT MAX(worker_salary) AS maximum_salary
+FROM workers;
+
+
+-- FIND THE TOTAL NUMBER OF EMPLOYEES (USE ALIAS);
+
+SELECT COUNT(worker_id) AS number_of_employees
+FROM workers;
+
+
+-- FIND THE AVERAGE SALARY AND USE ALIAS ....
+SELECT AVG(worker_salary) AS average_salary
+FROM workers;
+
+-- remove digits after comma .....
+SELECT ROUND(AVG(worker_salary), 1) AS average_salary
+FROM workers;
+
+
+
 
 
 SELECT * FROM workers;
+
+
+
 
 
 
