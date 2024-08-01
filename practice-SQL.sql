2bdb229b89a1603abae323f6c63f042c65161da7
olkasen765 <o.kassenov@eot.kz>
2024-08-01 02:50:27 -0500

    SQL practice

---
+++ practice-SQL.sql
@@ -0,0 +1,13 @@
+DROP TABLE practiceWeekdays;
+
+
+create table practiceWeekdays (
+id int,
+name varchar(50),
+city varchar(50),
+date varchar(50)
+);
+
+INSERT INTO practiceWeekdays VALUES (001, 'Thursday', 'Austin', '01-08-2024');
+
+SELECT * FROM practiceWeekdays;
