28cf862f5aaa4efe3c85afee21012adb59b14752
olkasen765 <o.kassenov@eot.kz>
2024-08-11 10:38:17 -0500

    SQL part

--- practice-SQL.sql
+++ practice-SQL.sql
@@ -10,5 +10,6 @@ date varchar(50)
 
 INSERT INTO practiceWeekdays VALUES (001, 'Thursday', 'Austin', '01-08-2024');
 INSERT INTO practiceWeekdays VALUES (002, 'Monday', 'Austin', '05-08-2024');
+INSERT INTO practiceWeekdays VALUES (003, 'Sunday', 'Austin', '11-08-2024');
 
 SELECT * FROM practiceWeekdays;
